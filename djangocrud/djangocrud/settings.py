d64fa839e022dab138eb3a227cd30cd29cef8f41
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2024-07-13 20:08:12 -0500

    Django - agregando cors-headers para corregir errores, ademas de registrarlos en settings.py del proyecto

--- djangocrud/djangocrud/settings.py
+++ djangocrud/djangocrud/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200"
 ]
 
 ROOT_URLCONF = 'djangocrud.urls'
